4a0c59fb21ecbb26e11e6f4d17cc62f452e3d8b2
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-15 20:56:57 -0800

    Final Commit

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Professor");
+            Console.WriteLine("Juan Hernandez, I want learn more about object oriented progaming.");
         }
     }
 }
